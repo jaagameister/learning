14c996f28dbab28642dde1aa47981a64f025a180
Freeman Murray <fcmurray@gmail.com>
2016-01-22 19:24:01 +0530

    fixed command loop

--- SimpleAddition100.java
+++ SimpleAddition100.java
@@ -5,48 +5,40 @@ interface Problem {
     public String getAnswer();
 }
 
-class SimpleAddition100Problem implements Problem {
-    static final int MAX = 100;
+class SimpleAddition100 implements Problem {
+    static final int MAX = 10000;
 
     String prompt;
     String answer;
+//    static Random random = new Random();
     
-    public SimpleAddition100Problem() {
-	int a = (int)Math.random() * MAX;
-	int b = (int)Math.random() * MAX;
-	prompt = new String(a + " + " + b + " = ?");
-	answer = new String(Integer.toString(a + b));
+    public SimpleAddition100() {
+		int a = new Random().nextInt();
+		int b = new Random().nextInt();;
+		prompt = new String(a + " + " + b + " = ?");
+		answer = new String(Integer.toString(a + b));
     }
 
     public String getPrompt() {
-	return prompt;
+		return prompt;
     }
 
     public String getAnswer() {
-	return answer;
+		return answer;
     }
 
     public static void main(String[] argv) {
-	Scanner sc = new Scanner(System.in);
-
-	Problem p = new SimpleAddition100Problem();
-	System.out.println(p.getPrompt());
-
-	String ans = null;
-	try {  
-	    ans = sc.next();
-	} catch (Exception e) {
-	    e.printStackTrace();
+		Scanner sc = new Scanner(System.in);
+		String ans = "";
+		while (!ans.equals("quit")) {
+			Problem p = new SimpleAddition100Problem();
+			System.out.println(p.getPrompt());
+			ans = sc.next();
+		    if (ans.equals(p.getAnswer())) {
+		        System.out.println("Correct");
+		    } else if (!ans.equals("quit")) {
+		        System.out.println("Sorry");
+		    }
+		}
 	}
-
-	while (!ans.equals("quit")) {
-	    if (ans.equals(p.getAnswer())) {
-	        System.out.println("Correct");
-	    } else {
-	        System.out.println("Sorry");
-	    }
-	    ans = sc.nextLine();
-	}
-	System.out.println("hello");
-    }
 }
