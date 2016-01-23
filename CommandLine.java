c9fb682be37b40046119e8555bcc730622adb569
Freeman Murray <fcmurray@gmail.com>
2016-01-23 15:09:07 +0530

    negative numbers

--- CommandLine.java
+++ CommandLine.java
@@ -15,9 +15,25 @@ public class CommandLine {
 
         int points = 0;
 		ArrayList<Skill> mission = new ArrayList<Skill>();
+		mission.add(new Skill(new Addition(10), 5, 100));
+		mission.add(new Skill(new Addition(100), 5, 150));
+		mission.add(new Skill(new Subtraction(10), 5, 100));
+		mission.add(new Skill(new Subtraction(100), 5, 150));
+
+		mission.add(new Skill(new Multiplication(5, 5), 10, 200));
+		mission.add(new Skill(new Multiplication(10, 10), 10, 200));
+		mission.add(new Skill(new Addition(1000), 5, 150));
+
+		mission.add(new Skill(new Division(30, 10), 5, 250));
+		mission.add(new Skill(new Division(100, 10), 5, 250));
+		mission.add(new Skill(new DivisionRemainders(30, 10), 5, 300));
+		mission.add(new Skill(new Subtraction(1000), 5, 100));
+		mission.add(new Skill(new Multiplication(100, 10), 10, 200));
+
+		mission.add(new Skill(new Addition(-10), 5, 100));
+		mission.add(new Skill(new Subtraction(-10), 5, 150));
 		mission.add(new Skill(new Multiplication(-12, 12), 10, 200));
-		mission.add(new Skill(new Subtraction(-10), 10, 150));
-		mission.add(new Skill(new Addition(-10), 10, 100));
+
 		mission.add(new Skill(new VariableDivision(100, 10), 8, 100));
 		mission.add(new Skill(new VariableMultiplication(10, 10), 8, 100));
 		mission.add(new Skill(new VariableSubtraction(10), 8, 100));
