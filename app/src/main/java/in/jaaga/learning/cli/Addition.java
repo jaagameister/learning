5ce681c7d1bd719046f121d7eb31131a8859609f
Freeman Murray <fcmurray@gmail.com>
2016-01-26 15:41:36 +0530

    negative numbers

--- app/src/main/java/in/jaaga/learning/cli/Addition.java
+++ app/src/main/java/in/jaaga/learning/cli/Addition.java
@@ -1,13 +1,18 @@
-package in.jaaga.learning.cli;
-
 import java.util.*;
 
 class Addition extends Problem {
 
     public Addition(int max) {
 		init(max);
-		int a = new Random().nextInt(max);
-		int b = new Random().nextInt(max);
+
+        int a,b;
+        if (max > 0) {
+			a = new Random().nextInt(max);
+			b = new Random().nextInt(max);
+        } else {  // max is negative
+            a = new Random().nextInt(max * -2) - (-max);
+            b = new Random().nextInt(max * -2) - (-max);
+        }       
 		setPrompt(new String(a + " + " + b + " = ?"));
 		setAnswer(new String(Integer.toString(a + b)));
     }
@@ -17,7 +22,11 @@ class Addition extends Problem {
     }
 
     public String getTitle() {
-    	return "Simple Addition to " + max1;
+        int m = Math.abs(max1);
+        String title = "Addition to " + max1; 
+        if (max1 > 0)
+            return title;
+        return title + " with negative numbers";
     }
 }
 
