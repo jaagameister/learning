2ed17940b9edff0f82ea7f3f9f701264d11f70f9
Freeman Murray <fcmurray@gmail.com>
2016-02-04 14:40:39 +0530

    cleaned up problem

--- app/src/main/java/in/jaaga/learning/Problem.java
+++ app/src/main/java/in/jaaga/learning/Problem.java
@@ -5,27 +5,6 @@ import java.lang.reflect.* ;
 public abstract class Problem {
     String prompt;
     String answer;
-    int max1 = -1;
-    int max2 = -1;
-    
-    public Problem() {}
-
-    public Problem(int max) {
-        init(max);
-    }
-
-    public Problem(int max1, int max2) {
-        init(max1, max2);
-    }
-
-    public void init(int max) {
-        max1 = max;
-    }
-
-    public void init(int max1, int max2) {
-        this.max1 = max1;
-        this.max2 = max2;
-    }
 
     public String getPrompt() {
     	return prompt;
@@ -47,16 +26,7 @@ public abstract class Problem {
         return answer;
     }
 
-    public Problem next() {
-        try {
-            Constructor<? extends Problem> constructor = 
-                getClass().getDeclaredConstructor(Integer.TYPE, Integer.TYPE) ;
-            return constructor.newInstance(max1, max2);
-        } catch( Exception e ) {
-            e.printStackTrace();
-        }
-        return null;
-    }
+    public abstract Problem next();
 
     public abstract String getTitle();
 }
