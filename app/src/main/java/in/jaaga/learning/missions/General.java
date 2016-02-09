f6326748c24917cab983ff843aacf019d2084325
harish <r.shivi@yahoo.com>
2016-02-09 12:52:27 +0530

    commit for untrack

--- app/src/main/java/in/jaaga/learning/missions/General.java
+++ app/src/main/java/in/jaaga/learning/missions/General.java
@@ -3,18 +3,7 @@ package in.jaaga.learning.missions;
 import java.util.ArrayList;
 
 import in.jaaga.learning.Skill;
-import in.jaaga.learning.problems.Addition;
-import in.jaaga.learning.problems.DecimalAddition;
-import in.jaaga.learning.problems.DecimalsDivision;
-import in.jaaga.learning.problems.Division;
-import in.jaaga.learning.problems.DivisionRemainders;
-import in.jaaga.learning.problems.Multiplication;
-//import in.jaaga.learning.problems.NumbersSequence;
-import in.jaaga.learning.problems.Subtraction;
-import in.jaaga.learning.problems.VariableAddition;
-import in.jaaga.learning.problems.VariableDivision;
-import in.jaaga.learning.problems.VariableMultiplication;
-import in.jaaga.learning.problems.VariableSubtraction;
+import in.jaaga.learning.problems.*;
 
 /**
  * Created by freeman on 4/2/16.
@@ -37,7 +26,7 @@ public class General implements Mission {
         //mission.add(new Skill(new Division(30, 10), 5, 250));
         mission.add(new Skill(new Division(100, 10), 5, 250));
         mission.add(new Skill(new DivisionRemainders(30, 10), 5, 300));
-        mission.add(new Skill(new DecimalsDivision(100,10),5,100));
+        mission.add(new Skill(new DecimalDivision(100,10),5,100));
         mission.add(new Skill(new Subtraction(1000), 5, 100));
         mission.add(new Skill(new Multiplication(100, 10), 10, 200));
 
