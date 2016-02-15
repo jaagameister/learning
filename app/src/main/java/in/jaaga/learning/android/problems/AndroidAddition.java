c3b41ded1eafd15cd6c1ed83af1e4aa13bf3dd2b
Freeman Murray <fcmurray@gmail.com>
2016-02-15 14:03:38 +0530

    make AndroidAddition extend Addition

--- app/src/main/java/in/jaaga/learning/android/problems/AndroidAddition.java
+++ app/src/main/java/in/jaaga/learning/android/problems/AndroidAddition.java
@@ -3,25 +3,16 @@ package in.jaaga.learning.android.problems;
 import java.util.*;
 
 import in.jaaga.learning.Problem;
+import in.jaaga.learning.problems.Addition;
 import in.jaaga.learning.problems.SimpleProblem;
 import in.jaaga.learning.R;
 import in.jaaga.learning.android.S;
 
-public class AndroidAddition extends SimpleProblem {
+public class AndroidAddition extends Addition {
     int max;
 
     public AndroidAddition(int max) {
-        this.max = max;
-        int a, b;
-        if (max > 0) {
-			a = new Random().nextInt(max);
-			b = new Random().nextInt(max);
-        } else {  // max is negative
-            a = new Random().nextInt(max * -2) - (-max);
-            b = new Random().nextInt(max * -2) - (-max);
-        }
-        prompt = a + " + " + b + " = ?";
-        answer = Integer.toString(a + b);
+        super(max);
     }
 
     public Problem next() {
