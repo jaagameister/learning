6afd055e7415217233d6f5a8eb3c641d9575d9db
Freeman Murray <fcmurray@gmail.com>
2016-02-15 17:20:14 +0530

    move RESPONSE TYPE to ChatItem

--- app/src/main/java/in/jaaga/learning/ChatItem.java
+++ app/src/main/java/in/jaaga/learning/ChatItem.java
@@ -4,9 +4,33 @@ package in.jaaga.learning;
  * Created by root on 24/1/16.
  */
 public class ChatItem {
+    public static final int NO_RESPONSE = 0;
+    public static final int NUMBER_RESPONSE = 1;
+    public static final int TEXT_RESPONSE = 2;
+
+    String sender;
     String message;
+    String attachment;
     int resourceId = -1;
     int responseType;
+
+    public ChatItem() {}
+
+    public ChatItem(String message) {
+        this.message = message;
+        sender = "sender";
+        responseType = NO_RESPONSE;
+    }
+
+    public ChatItem(String message, int responseType) {
+        this(message);
+        this.responseType = responseType;
+    }
+
+    public ChatItem(String message, String attachment, int responseType) {
+        this(message, responseType);
+        this.attachment = attachment;
+    }
 
     public String getSender() {
         return sender;
@@ -15,8 +39,6 @@ public class ChatItem {
     public void setSender(String sender) {
         this.sender = sender;
     }
-
-    String sender;
 
     public String getMessage() {
         return message;
