39ececcfbee1f4902ec959df249c400abace27f8
rajeef <rajeefmk@gmail.com>
2016-10-25 06:55:14 +0530

    feat: Style.xml updated, support libraries updated

---
+++ app/src/main/java/in/jaaga/learning/platform/util/DashboardActivity.java
@@ -0,0 +1,15 @@
+package in.jaaga.learning.platform.util;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import in.jaaga.learning.R;
+
+public class DashboardActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dashboard);
+    }
+}
